24bf9cc251296f5cbd402c61153ae3f0390b5906
Walker, Corey <corey_walker@mcafee.com>
2020-02-07 21:43:05 -0800

    Move structure; add comments and a README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # randomPassword
  Random Password Generator Assignment
+
+A click of a button, choose the length of the password, and select the password requirements. Out pops a randomly generated password!
+
+## Built With
+
+* HTML
+* CSS
+* JavaScript
+
+
+## Week 3 Requirements
+
+* Randomly generate a password that meets certain criteria
+* Create a strong password that provides greater security
+
+
+## Directions
+
+* Click a button to generate a password
+* Ask for password criteria
+    * Length of at least 8 characters and no more than 128 characters
+* Ask for character types to include in the password (at least one must be selected)
+    * Lowercase
+    * Uppercase
+    * Numeric
+    * Special Character
--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function generatePassword() {
   var genPassword = "";
   var passLenText = "";
   var passLength = 0;
+  // Prompt for the length of the password and then validate it until is within the limits
   do {
     passLenText = prompt("Please chose a length of at least 8 characters and no more than 128 characters.");
     passLength = parseInt(passLenText);
@@ -27,6 +28,7 @@ function generatePassword() {
   var charUppercase = false;
   var numPass = false;
   var charSpecial = false;
+  // Confirm each of the users choices and then validate that they have at least chosen one
   do {
     charLowercase = confirm("Whould you like to use lowercase characters?");
     charUppercase = confirm("Would you like to use uppercase characters?");
@@ -35,6 +37,7 @@ function generatePassword() {
   }
   while(!validateCharTypes(charLowercase, charUppercase, numPass, charSpecial));
   
+  // store the chosen password requirements
   var passwordArray = [];
 
   if(charLowercase) {
@@ -50,30 +53,32 @@ function generatePassword() {
     passwordArray.push("specialChar");
   }
 
+  // copy the password requirements to be eliminated later
   var typeCheck = passwordArray.slice();
 
   for (var i = passLength; i >= 1; i--){
+    // randomly choose a password type
     var charType = passwordArray[Math.floor(Math.random() * passwordArray.length)];
-    console.log("Random type: " + charType);
-    console.log("Types left: " + typeCheck);
     if(typeCheck.indexOf(charType) > -1){
+      // eliminate a password type if randomly chosen to ensure that they all get used
       typeCheck.splice(typeCheck.indexOf(charType), 1);
       genPassword += genRandomChar(charType);
     }else if(typeCheck.length === i){
+      // randomly choose a character based on the remaining password type
       genPassword += genRandomChar(typeCheck[i-1]);
       typeCheck.pop();
     }else{
       genPassword += genRandomChar(charType);
     }
     
-    debugPassword(charType, genPassword, i);
+    // debugPassword(charType, genPassword, i);
   }
 
   return genPassword;
 }
 
+// validate the length of the password
 function validatePassLength(passwordLen) {
-  console.log("Validate len: " + passwordLen);
   if(isNaN(passwordLen)){
     alert("Your response must be numeric.");
     return false;
@@ -84,6 +89,7 @@ function validatePassLength(passwordLen) {
   return true;
 }
 
+// validate type selection
 function validateCharTypes(lower, upper, num, special) {
   if(!lower && !upper && !num && !special) {
     alert("You must make at least one selection.");
@@ -92,6 +98,7 @@ function validateCharTypes(lower, upper, num, special) {
   return true;
 }
 
+// generate a random character
 function genRandomChar(rtnChar) {
   var charTypeArray = {
     charLower: "abcdefghijklmnopqrstuvwxyz",
@@ -99,7 +106,6 @@ function genRandomChar(rtnChar) {
     passwordNum: "0123456789",
     specialChar: "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   }
-  console.log("Return char: " + rtnChar);
   return charTypeArray[rtnChar].charAt(Math.floor(Math.random() * charTypeArray[rtnChar].length));
 }
 
